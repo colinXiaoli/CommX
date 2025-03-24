b59c90cfce1abe3056d9dbe83ab0222964b122e8
lishijie <conlinlii@163.com>
2025-03-24 23:13:29 +0800

    新建项目

---
+++ CommX/CommX/App.axaml.cs
@@ -0,0 +1,28 @@
+using Avalonia;
+using Avalonia.Controls.ApplicationLifetimes;
+using Avalonia.Markup.Xaml;
+using CommX.Views;
+using Prism.DryIoc;
+using Prism.Ioc;
+
+namespace CommX
+{
+    public partial class App : PrismApplication
+    {
+        public override void Initialize()
+        {
+            AvaloniaXamlLoader.Load(this);
+            base.Initialize(); 
+        }
+
+        protected override AvaloniaObject CreateShell()
+        {
+           return Container.Resolve<MainView>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+           
+        }
+    }
+}
---
+++ CommX/CommX/Views/MainView.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace CommX.Views;
+
+public partial class MainView : Window
+{
+    public MainView()
+    {
+        InitializeComponent();
+    }
+}
